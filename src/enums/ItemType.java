1936d0895f4001b30e7be3ba86596d6a8f156189
Ruud van den Boomen <ruudvdboomen@hotmail.com>
2016-09-26 15:14:31 +0200

    Opzet enums
    Begin opzet van de enums

---
+++ src/enums/ItemType.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enums;
+
+/**
+ *
+ * @author Ruud
+ */
+public enum ItemType {
+    Woodenplates,
+    Woodentick,
+    Woodensword,
+    Stonesword,
+    Steelsword,
+    Gemstonesword,
+    Woodenshield,
+    Stoneshield,
+    Steelshield,
+    Gemstoneshield,
+    Woodenshovel,
+    Stoneshovel,
+    Steelshovel,
+    Gemstoneshovel,
+    Woodenaxe,
+    Stoneaxe,
+    Steelaxe,
+    Gemstoneaxe,
+    Woodenpickaxe,
+    Stonepickaxe,
+    Steelpickaxe,
+    Gemstonepickaxe,
+    Steelplatebody,
+    Steelhelmet,
+    Steellegs,
+    Steelboots,
+    Leatherplatebody,
+    Leatherhelmet,
+    Leatherlegs,
+    Leatherboots,
+    Cowhide,
+    Wool, 
+    Gravel,
+    Sand,
+    Stone,
+    Wood,
+    Coal,
+    Steel,
+    Gemstone,
+    bucket
+}
