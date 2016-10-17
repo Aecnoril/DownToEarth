283d7ec29369e64d61f4eed16995f2fdf0e53b5a
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2016-10-17 09:04:43 +0200

    player

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -11,10 +11,11 @@ import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.geom.Point;
 import enums.*;
 import Items.*;
+import java.awt.event.KeyEvent;
 
 /**
  *
- * @author Demian
+ * @author Tomt
  */
 public class Player extends Entity{
     
@@ -24,6 +25,30 @@ public class Player extends Entity{
     private Item rightHand;
     private Item leftHand;
     private Item armorSet;
+    
+    private int dx;
+    private int dy;
+    private int y;
+    private int x;
+
+    /**
+     * Get the value of x
+     *
+     * @return the value of x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Get the value of y
+     *
+     * @return the value of y
+     */
+    public int getY() {
+        return y;
+    }
+
 
     /**
      * Get the value of thirst
@@ -77,9 +102,50 @@ public class Player extends Entity{
     }
     
     public void move(){
-        throw new UnsupportedOperationException("Not supported yet.");
+        x+= dx;
+        y+=dy;
+    }
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key==KeyEvent.VK_W)
+        {
+            dy = -1;
+        }
+        if (key==KeyEvent.VK_S)
+        {
+            dy=1;
+        }
+        if(key==KeyEvent.VK_A)
+        {
+            dx = -1;
+        }
+        if (key==KeyEvent.VK_D)
+        {
+            dx=1;
+        }
     }
     
+    public void keyReleased(KeyEvent e)
+    {
+        int key = e.getKeyCode();
+        if (key==KeyEvent.VK_W)
+        {
+            dy = 0;
+        }
+        if (key==KeyEvent.VK_S)
+        {
+            dy=0;
+        }
+        if(key==KeyEvent.VK_A)
+        {
+            dx = 0;
+        }
+        if (key==KeyEvent.VK_D)
+        {
+            dx=0;
+        }
+    }
+
     public void attack(){
         throw new UnsupportedOperationException("Not supported yet.");
     }
