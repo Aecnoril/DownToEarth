406b270846e570b8548f661f270936c4f6d8b64e
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-01-09 15:00:30 +0100

    soundmanager 2.0

---
+++ src/downtoearth/gameUtil/SoundManager.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package downtoearth.gameUtil;
+
+import downtoearth.entities.NPC;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+/**
+ *
+ * @author Tomt
+ */
+public class SoundManager {
+    
+    private InputStream in;
+    private AudioStream audioStream;
+
+    public SoundManager() {
+    }
+    
+    public void playSound(String path)
+    {
+        try {
+             in = new FileInputStream("C:\\Users\\Tomt\\Documents\\J2S1\\Proftaak\\Sfx\\Edit\\"+path);
+             audioStream = new AudioStream(in);
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(SoundManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        AudioPlayer.player.start (audioStream);
+    }  
+}
