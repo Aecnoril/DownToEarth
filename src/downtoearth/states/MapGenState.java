60fe7adeee8c9080beb12db5dcf4bdd7f3ebf3ce
Aecnoril <Aecnorilkoen@gmail.com>
2017-01-16 13:32:05 +0100

    work on map generation gamestate

--- src/downtoearth/states/MapGenState.java
+++ src/downtoearth/states/MapGenState.java
@@ -1,5 +1,8 @@
+package downtoearth.states;
 
+import downtoearth.gameUtil.Coordinate;
 import downtoearth.states.gui.Button;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -12,8 +15,11 @@ public class MapGenState extends BasicGameState {
 
     
     private Image backgroundImage;
+    private Image mapImage;
+    private Coordinate imageCoords;
     
     private int rebound;
+    
 
     public static void main(String[] args) {
         // TODO code application logic here
@@ -21,7 +27,7 @@ public class MapGenState extends BasicGameState {
 
     @Override
     public int getID() {
-        return 1;
+        return 6;
     }
 
     @Override
@@ -30,12 +36,23 @@ public class MapGenState extends BasicGameState {
         Point middle = new Point(container.getWidth()/2, container.getHeight()/2);
         
         backgroundImage = new Image("res/Backgrounds/MenuBackground.png");
+        mapImage = new Image("res/ColorMap.png");
+        
+        imageCoords = new Coordinate((container.getWidth() - container.getWidth()/3) - 64, 64);
+        
+        
+        
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         g.drawString("State 1: Menu", 0, 30);
         backgroundImage.draw(0, 0, container.getWidth(), container.getHeight());
+        g.setColor(Color.darkGray);
+        g.setLineWidth(6);
+        g.drawRect(imageCoords.getXint() - 3, imageCoords.getYint() - 3, container.getWidth()/3 + 6, container.getHeight()/3 + 6);
+        mapImage.draw(imageCoords.getXint(), imageCoords.getYint(), container.getWidth()/3, container.getHeight()/3);
+        //mapImage.draw(container.getWidth() - mapImage.getWidth()/4 , 128, container.getWidth()/4, container.getHeight()/4);
     }
 
     @Override
